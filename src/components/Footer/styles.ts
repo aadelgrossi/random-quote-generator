78dc2a5426f4e3fbce28172acb1b4d0f7715fc15
Andre Grossi <andredg91@gmail.com>
2020-07-28 19:26:24 -0300

    Create and style footer

---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin: auto 0 20px;
+
+  p {
+    color: #a9a9a9;
+    font-family: 'Montserrat';
+    font-size: 14px;
+  }
+`;
