a9a8ddb442b9533358617f303209377e602301d2
Andre Grossi <andredg91@gmail.com>
2020-07-28 19:26:37 -0300

    Add components and pages in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
+import './assets/fonts';
+import GlobalStyle from './styles/global';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+
 const App: React.FC = () => {
-  return <div />;
+  return (
+    <>
+      <Header />
+      <Home />
+      <Footer />
+      <GlobalStyle />
+    </>
+  );
 };
 
 export default App;
