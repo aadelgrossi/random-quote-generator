e6dd80cadeb84c74ae1f33454289ce4d9ed3ba53
Andre Grossi <andredg91@gmail.com>
2020-07-31 19:29:47 -0300

    Add test to verify redirect to author quotes page when clicking on author name

--- src/__tests__/App.tsx
+++ src/__tests__/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, act } from '@testing-library/react';
+import { render, act, fireEvent } from '@testing-library/react';
 import MockAdapter from 'axios-mock-adapter';
 
 import api from '../services/api';
@@ -40,5 +40,48 @@ describe('Home', () => {
     );
     expect(getByTestId('quote-author')).toHaveTextContent('Charles Phillips');
     expect(getByTestId('quote-genre')).toHaveTextContent('strength');
+  });
+
+  it('should be able to navigate to author quotes page', async () => {
+    const { getByTestId } = render(<App />);
+
+    apiMock.onGet('/quotes/random').reply(200, {
+      statusCode: 200,
+      quote: {
+        _id: '5eb17ab3b69dc744b4e80ba5',
+        quoteText:
+          "They just didn't have the sense of the strength of their vote. Just thought it wasn't necessary.",
+        quoteAuthor: 'Charles Phillips',
+        quoteGenre: 'strength',
+        __v: 0,
+      },
+    });
+
+    apiMock.onGet(`/authors/Charles Phillips?page=1&limit=5`).reply(200, {
+      statusCode: 200,
+      message: 'Quotes by Charles Phillips',
+      totalPages: 15619,
+      currentPage: 1,
+      quotes: [
+        {
+          _id: '5eb17ab3b69dc744b4e80ba5',
+          quoteText:
+            "They just didn't have the sense of the strength of their vote. Just thought it wasn't necessary.",
+          quoteAuthor: 'Charles Phillips',
+          quoteGenre: 'strength',
+          __v: 0,
+        },
+      ],
+    });
+
+    await actWait(500);
+
+    fireEvent.click(getByTestId('author-button'));
+
+    await actWait();
+
+    expect(window.location.pathname).toEqual(
+      encodeURI('/authors/Charles Phillips'),
+    );
   });
 });
