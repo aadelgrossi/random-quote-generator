7167bb467344e7b4e394f7ba307d15659065381e
Andre Grossi <andredg91@gmail.com>
2020-08-01 18:33:11 -0300

    Add test to assert quote is updated when clicking "random" header button

--- src/__tests__/App.tsx
+++ src/__tests__/App.tsx
@@ -18,10 +18,14 @@ const actWait = async (amount = 0): Promise<void> => {
 };
 
 describe('Home', () => {
+  afterEach(() => {
+    apiMock.resetHistory();
+  });
+
   it('should be able to render a random quote', async () => {
     const { getByTestId } = render(<App />);
 
-    apiMock.onGet('/quotes/random').reply(200, {
+    apiMock.onGet('/quotes/random').replyOnce(200, {
       statusCode: 200,
       quote: {
         _id: '5eb17ab3b69dc744b4e80ba5',
@@ -42,10 +46,54 @@ describe('Home', () => {
     expect(getByTestId('quote-genre')).toHaveTextContent('strength');
   });
 
+  it('should fetch and render a new quote when header button is pressed', async () => {
+    const { getByTestId, getByText } = render(<App />);
+
+    apiMock.onGet('/quotes/random').replyOnce(200, {
+      statusCode: 200,
+      quote: {
+        _id: '5eb17aaeb69dc744b4e74356',
+        quoteText:
+          "As a kid, all I thought about was death. But you can't tell your parents that.",
+        quoteAuthor: 'Maurice Sendak',
+        quoteGenre: 'death',
+        __v: 0,
+      },
+    });
+
+    await actWait(500);
+
+    expect(getByTestId('quote-content')).toHaveTextContent(
+      "As a kid, all I thought about was death. But you can't tell your parents that.",
+    );
+
+    apiMock.onGet('/quotes/random').replyOnce(200, {
+      statusCode: 200,
+      quote: {
+        _id: '5eb17aadb69dc744b4e72248',
+        quoteText:
+          'I always say you can never be extravagant with beauty. Beauty is God made real. Beauty is life.',
+        quoteAuthor: 'Imelda Marcos',
+        quoteGenre: 'beauty',
+        __v: 0,
+      },
+    });
+
+    await actWait(500);
+
+    fireEvent.click(getByText('random'));
+
+    await actWait();
+
+    expect(getByTestId('quote-content')).toHaveTextContent(
+      'I always say you can never be extravagant with beauty. Beauty is God made real. Beauty is life.',
+    );
+  });
+
   it('should be able to navigate to author quotes page', async () => {
     const { getByTestId } = render(<App />);
 
-    apiMock.onGet('/quotes/random').reply(200, {
+    apiMock.onGet('/quotes/random').replyOnce(200, {
       statusCode: 200,
       quote: {
         _id: '5eb17ab3b69dc744b4e80ba5',
@@ -57,7 +105,7 @@ describe('Home', () => {
       },
     });
 
-    apiMock.onGet(`/authors/Charles Phillips?page=1&limit=5`).reply(200, {
+    apiMock.onGet('/authors/Charles Phillips?page=1&limit=5').replyOnce(200, {
       statusCode: 200,
       message: 'Quotes by Charles Phillips',
       totalPages: 15619,
