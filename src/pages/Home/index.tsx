01ec4c0240acdc27d8bd3c88b35d9d13d5b50fe4
Andre Grossi <andredg91@gmail.com>
2020-08-05 11:19:29 -0300

    Add and render loading component in pages

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,21 +5,31 @@ import { useQuote } from '../../hooks/quote';
 import Quote from '../../components/Quote';
 
 import { Container, Author } from './styles';
+import Loading from '../../components/LoadingDots';
 
 const Home: React.FC = () => {
-  const { randomQuote } = useQuote();
+  const { randomQuote, loading } = useQuote();
 
   return (
     <Container>
-      <Quote content={randomQuote.text} />
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          <Quote content={randomQuote.text} />
 
-      <Author data-testid="author-button" to={`/authors/${randomQuote.author}`}>
-        <span>
-          <h3 data-testid="quote-author">{randomQuote.author}</h3>
-          <span data-testid="quote-genre">{randomQuote.genre}</span>
-        </span>
-        <MdTrendingFlat size="1.5em" />
-      </Author>
+          <Author
+            data-testid="author-button"
+            to={`/authors/${randomQuote.author}`}
+          >
+            <span>
+              <h3 data-testid="quote-author">{randomQuote.author}</h3>
+              <span data-testid="quote-genre">{randomQuote.genre}</span>
+            </span>
+            <MdTrendingFlat size="1.5em" />
+          </Author>
+        </>
+      )}
     </Container>
   );
 };
