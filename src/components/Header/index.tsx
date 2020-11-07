d4380716df29d5904a9bfa8ba04c699114f6fb92
Andre Grossi <iamandregrossi@gmail.com>
2020-11-07 01:04:24 -0300

    Add react-switch and configure initial

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,15 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useContext } from 'react';
 
 import { MdCached } from 'react-icons/md';
 import { useLocation, useHistory } from 'react-router-dom';
+import Switch from 'react-switch';
+import { ThemeContext } from 'styled-components';
+import { FaMoon, FaSun } from 'react-icons/fa';
 import { Container } from './styles';
 import { useQuote } from '../../hooks/quote';
 
 const Header: React.FC = () => {
+  const { colors } = useContext(ThemeContext);
   const { getRandom } = useQuote();
   const history = useHistory();
   const location = useLocation();
@@ -18,8 +22,43 @@ const Header: React.FC = () => {
     }
   }, [history, location, getRandom]);
 
+  const handleThemeSwitch = useCallback(() => {}, []);
+
   return (
     <Container>
+      <Switch
+        onChange={handleThemeSwitch}
+        checked
+        onHandleColor={colors.primary}
+        onColor={colors.secondary}
+        width={60}
+        uncheckedIcon={
+          // eslint-disable-next-line react/jsx-wrap-multilines
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              height: '100%',
+            }}
+          >
+            <FaSun color={colors.primary} />
+          </div>
+        }
+        checkedIcon={
+          // eslint-disable-next-line react/jsx-wrap-multilines
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              height: '100%',
+            }}
+          >
+            <FaMoon color={colors.primary} />
+          </div>
+        }
+      />
       <button type="button" onClick={handleClick}>
         random
         <MdCached />
