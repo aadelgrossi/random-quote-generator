448afc51790d64e89e937d64b4595058b6b94cce
Andre Grossi <iamandregrossi@gmail.com>
2020-11-07 15:10:27 -0300

    Create Dark and LightIcon components

---
+++ src/components/DarkIcon/index.tsx
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react';
+import { FaMoon } from 'react-icons/fa';
+import { ThemeContext } from 'styled-components';
+
+import { Container } from './styles';
+
+const DarkIcon: React.FC = () => {
+  const { colors } = useContext(ThemeContext);
+  return (
+    <Container>
+      <FaMoon color={colors.primary} />
+    </Container>
+  );
+};
+
+export default DarkIcon;
