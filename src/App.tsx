aac8a7cd61382d02606d19fc31caa57bea4c17f9
Andre Grossi <iamandregrossi@gmail.com>
2020-11-07 15:29:24 -0300

    Create usePersistedState hook to store and retrieve theme info from localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback } from 'react';
 
 import './assets/fonts';
 import { BrowserRouter } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
+import { DefaultTheme, ThemeProvider } from 'styled-components';
 import GlobalStyle from './styles/global';
 
 import Header from './components/Header';
@@ -12,13 +12,14 @@ import Routes from './routes';
 import { QuoteProvider } from './hooks/quote';
 import light from './styles/themes/light';
 import dark from './styles/themes/dark';
+import usePersistedState from './hooks/usePersistedState';
 
 const App: React.FC = () => {
-  const [theme, setTheme] = useState(light);
+  const [theme, setTheme] = usePersistedState<DefaultTheme>('theme', light);
 
   const toggleTheme = useCallback(() => {
     setTheme(theme.title === 'light' ? dark : light);
-  }, [theme]);
+  }, [theme, setTheme]);
 
   return (
     <>
