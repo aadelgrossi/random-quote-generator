b1c9d1d3afd29b4bf7b808ed9c507f5efe5996c6
Andre Grossi <iamandregrossi@gmail.com>
2020-11-07 01:59:50 -0300

    Undo main provider to use ThemeProvider in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 
 import './assets/fonts';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './styles/global';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 import Routes from './routes';
-import AppProvider from './hooks';
+import { QuoteProvider } from './hooks/quote';
+import light from './styles/themes/light';
+import dark from './styles/themes/dark';
 
 const App: React.FC = () => {
+  const [theme, setTheme] = useState(light);
+
+  const toggleTheme = useCallback(() => {
+    setTheme(theme.title === 'light' ? dark : light);
+  }, [theme]);
+
   return (
     <>
       <BrowserRouter>
-        <AppProvider>
-          <Header />
-          <Routes />
-        </AppProvider>
-        <Footer />
+        <ThemeProvider theme={theme}>
+          <QuoteProvider>
+            <Header toggleTheme={toggleTheme} />
+            <Routes />
+          </QuoteProvider>
+          <Footer />
+        </ThemeProvider>
       </BrowserRouter>
       <GlobalStyle />
     </>
