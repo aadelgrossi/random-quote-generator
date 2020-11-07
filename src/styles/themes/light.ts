da3d0246b53dbd757752f7d5d33409e7355e9577
Andre Grossi <iamandregrossi@gmail.com>
2020-11-07 00:32:06 -0300

    Create light theme and type definitions

---
+++ src/styles/themes/light.ts
@@ -0,0 +1,12 @@
+export default {
+  title: 'light',
+  colors: {
+    primary: '#000',
+    secondary: '#4f4f4f',
+    tertiary: '#828282',
+    accent: '#f7df94',
+    white: '#f2f2f2',
+    background: '#fff',
+    hoverBackground: '#333',
+  },
+};
